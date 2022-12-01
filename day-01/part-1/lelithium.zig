16f140dfa39cac4332575db0051bfa65e5473d95
Louis Cabrol <louis@cabrol.xyz>
2022-12-01 09:18:38 +0100

    feat: lelithium d1 zig

---
+++ day-01/part-1/lelithium.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+
+var a: std.mem.Allocator = undefined;
+const stdout = std.io.getStdOut().writer(); //prepare stdout to write in
+
+fn run(input: [:0]const u8) u64 {
+    var elves_it = std.mem.split(u8, input, "\n\n");
+    var max: u64 = 0;
+    var cur: u64 = 0;
+    while (elves_it.next()) |elf| {
+        cur = 0;
+        var cal_it = std.mem.tokenize(u8, elf, "\n");
+        while (cal_it.next()) |cal_str| {
+            cur += std.fmt.parseInt(u32, cal_str, 10) catch unreachable;
+        }
+        if (cur > max){
+            max = cur;
+        }
+    }
+    return max;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator); // create memory allocator for strings
+
+    defer arena.deinit(); // clear memory
+    a = arena.allocator();
+
+    var arg_it = try std.process.argsWithAllocator(a);
+    _ = arg_it.skip(); // skip over exe name
+    const input: [:0]const u8 = arg_it.next().?;
+
+    const start: i128 = std.time.nanoTimestamp(); // start time
+    const answer = run(input); // compute answer
+    const elapsed_nano: f128 = @intToFloat(f128, std.time.nanoTimestamp() - start);
+    const elapsed_milli: f64 = @floatCast(f64, @divFloor(elapsed_nano, 1_000_000));
+    try stdout.print("_duration:{d}\n{}\n", .{ elapsed_milli, answer }); // emit actual lines parsed by AOC
+}
