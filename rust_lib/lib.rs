ca595b7454fa19dcbee417a343dccaa193fd52d1
paullgdc <paul.legranddec@gmail.com>
2022-12-02 11:16:26 +0100

    Move rust lib to rust_lib

--- rust_lib/lib.rs
+++ rust_lib/lib.rs
@@ -1,12 +1,11 @@
-use std::{time::Instant, env::args, fmt};
+use std::{fmt::Display, time::Instant, env::args};
 
 pub mod paullgdc;
 
-pub fn run<R: fmt::Display, F: FnOnce(&str) -> R>(f: F){
+pub fn run<R: Display, F: FnOnce(&str) -> R>(f: F){
     let now = Instant::now();
     let output = f(&args().nth(1).expect("Please provide an input"));
     let elapsed = now.elapsed();
     println!("_duration:{}", elapsed.as_secs_f64() * 1000.);
     println!("{}", output);    
 }
-
