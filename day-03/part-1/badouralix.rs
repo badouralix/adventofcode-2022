faefd71a82ebe207d1f24efc5b9ada0770b21a67
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-12-03 13:26:31 +0100

    [day-03] make badouralix dumb and fast

--- day-03/part-1/badouralix.rs
+++ day-03/part-1/badouralix.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 fn main() {
     aoc::run(run)
 }
@@ -8,12 +6,12 @@ fn run(input: &str) -> isize {
     let mut result = 0;
 
     for line in input.split('\n') {
-        let compartment: HashSet<char> = HashSet::from_iter(line[..line.len() / 2].chars());
-        for item in line[line.len() / 2..].chars() {
-            if compartment.contains(&item) {
+        let compartment = line[..line.len() / 2].as_bytes();
+        for item in line[line.len() / 2..].as_bytes() {
+            if compartment.contains(item) {
                 match item {
-                    'a'..='z' => result += item as isize - 'a' as isize + 1,
-                    'A'..='Z' => result += item as isize - 'A' as isize + 27,
+                    b'a'..=b'z' => result += (*item - b'a' + 1) as isize,
+                    b'A'..=b'Z' => result += (*item - b'A' + 27) as isize,
                     _ => todo!(),
                 }
                 break;
--- day-03/part-2/badouralix.rs
+++ day-03/part-2/badouralix.rs
@@ -1,25 +1,23 @@
-use std::collections::HashSet;
-
 fn main() {
     aoc::run(run)
 }
 
 fn run(input: &str) -> isize {
     let mut result = 0;
-    let mut group: Vec<HashSet<char>> = Vec::new();
+    let mut group = Vec::new();
 
     for line in input.split('\n') {
-        group.push(HashSet::from_iter(line.chars()));
+        group.push(line);
 
         if group.len() < 3 {
             continue;
         }
 
-        for item in &group[0] {
+        for item in group[0].chars() {
             if group[1].contains(item) && group[2].contains(item) {
                 match item {
-                    'a'..='z' => result += *item as isize - 'a' as isize + 1,
-                    'A'..='Z' => result += *item as isize - 'A' as isize + 27,
+                    'a'..='z' => result += item as isize - 'a' as isize + 1,
+                    'A'..='Z' => result += item as isize - 'A' as isize + 27,
                     _ => todo!(),
                 }
                 break;
