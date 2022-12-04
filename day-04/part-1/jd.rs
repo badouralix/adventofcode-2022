7cf72806094ca75e4abbe891e7026178eae682e7
Julien Doutre <julien.doutre@datadoghq.com>
2022-12-04 22:46:31 +0100

    Day 4

---
+++ day-04/part-1/jd.rs
@@ -0,0 +1,52 @@
+fn main() {
+    aoc::run(run)
+}
+
+fn run(input: &str) -> usize {
+    input.lines().fold(0, |acc, line| acc + full_overlap(line))
+}
+
+fn full_overlap(line: &str) -> usize {
+    let ((a, b), (c, d)) = parse_ranges(line);
+
+    if a >= c && b <= d {
+        1
+    } else if a <= c && b >= d {
+        1
+    } else {
+        0
+    }
+}
+
+fn parse_ranges(ranges: &str) -> ((usize, usize), (usize, usize)) {
+    let intervals = ranges.split(',').collect::<Vec<&str>>();
+
+    (parse_range(intervals[0]), parse_range(intervals[1]))
+}
+
+fn parse_range(range: &str) -> (usize, usize) {
+    let interval = range
+        .split('-')
+        .map(|e| e.parse::<usize>().unwrap_or_default())
+        .collect::<Vec<usize>>();
+
+    (interval[0], interval[1])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(
+            run("2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"),
+            2
+        )
+    }
+}
