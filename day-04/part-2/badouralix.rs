c4a3a1618bda8660b9daee56288f91ce50ff9388
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-12-04 11:50:48 +0100

    [day-04] add badouralix

---
+++ day-04/part-2/badouralix.rs
@@ -0,0 +1,54 @@
+use std::isize;
+
+fn main() {
+    aoc::run(run)
+}
+
+fn run(input: &str) -> isize {
+    let mut result = 0;
+
+    for line in input.split('\n') {
+        let (left, right) = match line.split_once(',') {
+            None => continue,
+            Some((left, right)) => (left, right),
+        };
+
+        let (left_min, left_max): (i64, i64) = match left.split_once('-') {
+            None => continue,
+            Some((min, max)) => (min.parse().unwrap(), max.parse().unwrap()),
+        };
+
+        let (right_min, right_max): (i64, i64) = match right.split_once('-') {
+            None => continue,
+            Some((min, max)) => (min.parse().unwrap(), max.parse().unwrap()),
+        };
+
+        if (left_min <= right_max && right_min <= left_max)
+            || (right_min <= left_max && left_min <= right_max)
+        {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(
+            run("
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
+                .trim()),
+            4
+        )
+    }
+}
