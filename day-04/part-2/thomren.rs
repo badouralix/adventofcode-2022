828e7d0beca2c8d246a892adb45799e5aa241b3b
Thomas Renault <thomas.renault56@gmail.com>
2022-12-04 10:11:17 +0100

    day 4 v2.1

--- day-04/part-2/thomren.rs
+++ day-04/part-2/thomren.rs
@@ -12,9 +12,7 @@ fn run(input: &str) -> usize {
         .map(|line| AssignementPair::from_str(line).unwrap())
         .filter(|AssignementPair(a, b)| 
             b.0 <= a.0 && a.0 <= b.1 || 
-            b.0 <= a.1 && a.1 <= b.1 || 
-            a.0 <= b.0 && b.0 <= a.1 || 
-            a.0 <= b.1 && b.1 <= a.1
+            a.0 <= b.0 && b.0 <= a.1
         )
         .count()
 }
