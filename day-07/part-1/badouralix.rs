20edb4665f33b35289d6aa96aee1e802ce4242a1
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-12-08 01:28:55 +0100

    [day-07] add badouralix with raw pointers

--- day-07/part-1/badouralix.rs
+++ day-07/part-1/badouralix.rs
@@ -1,9 +1,130 @@
-use std::{collections::HashMap, rc::Rc};
+use std::collections::HashMap;
 
+#[derive(Debug)]
 struct Filesystem {
-    files: Option<HashMap<String, Rc<Filesystem>>>,
-    parent: Option<Rc<Filesystem>>,
+    files: Option<HashMap<String, *mut Filesystem>>,
+    parent: Option<*mut Filesystem>,
     size: Option<isize>,
+}
+
+impl Filesystem {
+    fn new_from_input(input: &str) -> Filesystem {
+        let mut root = Filesystem {
+            files: Some(HashMap::new()),
+            parent: None,
+            size: None,
+        };
+
+        // The variable must be initialized as the rust compiler has no idea the first line of the input is `$ cd /`.
+        let mut pwd: *mut Filesystem = &mut root;
+
+        for line in input.split('\n') {
+            println!("{}", line);
+            unsafe {
+                println!("{:?}", *pwd);
+            }
+
+            match line.chars().next() {
+                Some('$') => {
+                    if line == "$ cd /" {
+                        // Same as the initial assignment of pwd.
+                        pwd = &mut root;
+                    } else if line == "$ cd .." {
+                        // Required unsafe to dereference pwd raw pointer.
+                        unsafe {
+                            if let Some(parent) = (*pwd).parent {
+                                pwd = parent
+                            }
+                        }
+                    } else if line.starts_with("$ cd ") {
+                        let dirname = String::from(line)[5..].to_string();
+                        println!("{}", dirname);
+
+                        // Required unsafe to dereference pwd raw pointer.
+                        unsafe {
+                            // We need to borrow with as_ref() as we cannot move out data behind a raw pointer.
+                            pwd = (*pwd).files.as_ref().unwrap()[&dirname];
+                        }
+                    } else if line == "$ ls" {
+                        continue;
+                    }
+                }
+
+                Some(_) => {
+                    if line.starts_with("dir") {
+                        let dirname = String::from(line)[4..].to_string();
+                        println!("found dir '{}'", dirname);
+
+                        // Required unsafe to dereference pwd raw pointer.
+                        unsafe {
+                            // We need to borrow with as_mut() as we cannot move out data behind a raw pointer.
+                            (*pwd).files.as_mut().unwrap().insert(
+                                dirname,
+                                &mut Filesystem {
+                                    files: Some(HashMap::new()),
+                                    parent: Some(pwd),
+                                    size: None,
+                                },
+                            );
+                        }
+
+                        unsafe {
+                            println!("{:?}", *pwd);
+                        }
+                    } else {
+                        let mut split = line.split(' ');
+                        let size: isize = split.next().unwrap().parse().unwrap();
+                        let filename = split.next().unwrap().to_string();
+                        println!("found file '{}'", filename);
+
+                        // Required unsafe to dereference pwd raw pointer.
+                        unsafe {
+                            // We need to borrow with as_mut() as we cannot move out data behind a raw pointer.
+                            (*pwd).files.as_mut().unwrap().insert(
+                                filename,
+                                &mut Filesystem {
+                                    files: None,
+                                    parent: None,
+                                    size: Some(size),
+                                },
+                            );
+                        }
+                    }
+                }
+
+                None => unimplemented!(),
+            }
+        }
+
+        root
+    }
+
+    fn total_size_of_at_most(&self, max_size: isize) -> isize {
+        if let Some(size) = self.size {
+            if size <= max_size {
+                size
+            } else {
+                0
+            }
+        } else {
+            let mut size = 0;
+            for item in self.files.as_ref().unwrap().values() {
+                unsafe {
+                    size += (*(*item)).total_size_of_at_most(max_size);
+                }
+
+                if size > max_size {
+                    return 0;
+                }
+            }
+
+            if size <= max_size {
+                size
+            } else {
+                0
+            }
+        }
+    }
 }
 
 fn main() {
@@ -11,75 +132,8 @@ fn main() {
 }
 
 fn run(input: &str) -> isize {
-    // Wrap root into an rc so that it is allocated on the heap and can be referenced multiple times.
-    // Options everywhere to distinguish files and dirs.
-    let root = Rc::new(Filesystem {
-        files: Some(HashMap::new()),
-        parent: None,
-        size: None,
-    });
-
-    // Here is the first reference to root. The rc is cloned without touching the underlying data.
-    // The variable must be initialized as the rust compiler has no idea the first line of the input is `$ cd /`.
-    let mut pwd = Rc::clone(&root);
-
-    for line in input.split('\n') {
-        match line.chars().next() {
-            Some('$') => {
-                if line == "$ cd /" {
-                    // Same as the initial assignment of pwd.
-                    pwd = Rc::clone(&root);
-                } else if line == "$ cd .." {
-                    // Apparently the borrowing with & is required here because Rc<Filesystem> does not implement Copy.
-                    if let Some(parent) = &pwd.parent {
-                        pwd = Rc::clone(parent)
-                    }
-                } else if line.starts_with("$ cd ") {
-                    let dirname = String::from(line)[5..].to_string();
-
-                    // We need to borrow the type's content with as_ref() as we cannot move out of an rc.
-                    pwd = Rc::clone(&pwd.files.as_ref().unwrap()[&dirname]);
-                } else if line == "$ ls" {
-                    continue;
-                }
-            }
-
-            Some(_) => {
-                if line.starts_with("dir") {
-                    let dirname = String::from(line)[5..].to_string();
-
-                    // Here we are stuck and likely need a refcell...
-                    // https://stackoverflow.com/questions/58599539/cannot-borrow-in-a-rc-as-mutable
-                    pwd.files.as_mut().unwrap().insert(
-                        dirname,
-                        Rc::new(Filesystem {
-                            files: Some(HashMap::new()),
-                            parent: Some(Rc::clone(&pwd)),
-                            size: None,
-                        }),
-                    );
-                } else {
-                    let mut split = line.split(' ');
-                    let size: isize = split.next().unwrap().parse().unwrap();
-                    let filename = split.next().unwrap().to_string();
-
-                    // Here we are stuck and likely need a refcell...
-                    // https://stackoverflow.com/questions/58599539/cannot-borrow-in-a-rc-as-mutable
-                    pwd.files.as_mut().unwrap().insert(
-                        filename,
-                        Rc::new(Filesystem {
-                            files: None,
-                            parent: None,
-                            size: Some(size),
-                        }),
-                    );
-                }
-            }
-
-            None => unimplemented!(),
-        }
-    }
-    0
+    let root = Filesystem::new_from_input(input);
+    root.total_size_of_at_most(100_000)
 }
 
 #[cfg(test)]
