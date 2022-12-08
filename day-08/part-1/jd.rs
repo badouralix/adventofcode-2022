b855f6c7d0ea62ed73e8077f676c87906242986c
Julien Doutre <julien.doutre@datadoghq.com>
2022-12-08 23:37:09 +0100

    jpp

---
+++ day-08/part-1/jd.rs
@@ -0,0 +1,77 @@
+fn main() {
+    aoc::run(run)
+}
+
+const SIZE: usize = 99;
+
+fn run(input: &str) -> usize {
+    process(input, SIZE)
+}
+
+fn process(input: &str, size: usize) -> usize {
+    let mut map = [[0; SIZE]; SIZE];
+
+    for (row, line) in input.lines().enumerate() {
+        for (col, tree) in line.chars().enumerate() {
+            let height = tree.to_digit(10).unwrap_or_default() as usize;
+            map[row][col] = height;
+        }
+    }
+
+    let mut visible_trees = 4 * size - 4;
+
+    for row in 1..size - 1 {
+        for col in 1..size - 1 {
+            let tree = map[row][col];
+
+            let left = map[row][..col].iter().max().copied().unwrap_or_default();
+            let right = map[row][col + 1..]
+                .iter()
+                .max()
+                .copied()
+                .unwrap_or_default();
+
+            let mut top = 0;
+            for l in map.iter().take(row) {
+                let e = l[col];
+                if e > top {
+                    top = e;
+                }
+            }
+
+            let mut bottom = 0;
+            for l in map.iter().take(size).skip(row + 1) {
+                let e = l[col];
+                if e > bottom {
+                    bottom = e;
+                }
+            }
+
+            if tree > left || tree > right || tree > top || tree > bottom {
+                visible_trees += 1;
+            }
+        }
+    }
+
+    visible_trees
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(
+            process(
+                "30373
+25512
+65332
+33549
+35390",
+                5
+            ),
+            21
+        )
+    }
+}
---
+++ day-08/part-2/jd.rs
@@ -0,0 +1,88 @@
+fn main() {
+    aoc::run(run)
+}
+
+const SIZE: usize = 99;
+
+fn run(input: &str) -> usize {
+    process(input, SIZE)
+}
+
+fn process(input: &str, size: usize) -> usize {
+    let mut map = [[0; SIZE]; SIZE];
+
+    for (row, line) in input.lines().enumerate() {
+        for (col, tree) in line.chars().enumerate() {
+            let height = tree.to_digit(10).unwrap_or_default() as usize;
+            map[row][col] = height;
+        }
+    }
+
+    let mut score = 0;
+
+    for row in 1..size - 1 {
+        for col in 1..size - 1 {
+            let tree = map[row][col];
+
+            let mut left_score = 0;
+            for j in (0..col).rev() {
+                left_score += 1;
+                if tree <= map[row][j] {
+                    break;
+                }
+            }
+
+            let mut right_score = 0;
+            for j in col + 1..size {
+                right_score += 1;
+                if tree <= map[row][j] {
+                    break;
+                }
+            }
+
+            let mut top_score = 0;
+            for i in (0..row).rev() {
+                top_score += 1;
+                if tree <= map[i][col] {
+                    break;
+                }
+            }
+
+            let mut bottom_score = 0;
+            for i in map.iter().take(size).skip(row + 1) {
+                bottom_score += 1;
+                if tree <= i[col] {
+                    break;
+                }
+            }
+
+            let tree_score = left_score * right_score * top_score * bottom_score;
+
+            if tree_score > score {
+                score = tree_score;
+            }
+        }
+    }
+
+    score
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(
+            process(
+                "30373
+25512
+65332
+33549
+35390",
+                5
+            ),
+            8
+        )
+    }
+}
