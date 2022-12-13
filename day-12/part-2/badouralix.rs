b364400a702827d31c89c2be365cb35982bdea99
Ayaz Badouraly <badouralix@users.noreply.github.com>
2022-12-13 01:27:33 +0100

    [day-12] boost badouralix - part 2

--- day-12/part-2/badouralix.rs
+++ day-12/part-2/badouralix.rs
@@ -7,6 +7,7 @@ struct Map {
     rows: Vec<Vec<u8>>,
     height: usize,
     width: usize,
+    start: (usize, usize),
     end: (usize, usize),
 }
 
@@ -23,6 +24,10 @@ impl Map {
             .collect();
         let height = input.matches('\n').count() + 1;
         let width = input.chars().position(|c| c == '\n').unwrap();
+        let start = (
+            input.chars().position(|c| c == 'S').unwrap() / (width + 1),
+            input.chars().position(|c| c == 'S').unwrap() % (width + 1),
+        );
         let end = (
             input.chars().position(|c| c == 'E').unwrap() / (width + 1),
             input.chars().position(|c| c == 'E').unwrap() % (width + 1),
@@ -32,62 +37,68 @@ impl Map {
             rows,
             height,
             width,
+            start,
             end,
         }
     }
 
-    fn find_all_start_positions(&self) -> Vec<(usize, usize)> {
-        let mut positions = Vec::new();
-        for i in 0..self.height {
-            for j in 0..self.width {
-                if self.rows[i][j] == b'a' {
-                    positions.push((i, j));
-                }
-            }
-        }
-        positions
-    }
-
-    fn find_shortest_path(&self, start: (usize, usize)) -> Option<usize> {
+    fn find_shortest_path(&self) -> usize {
         let mut distances = vec![vec![None; self.width]; self.height];
         let mut queue = VecDeque::<((usize, usize), usize)>::new();
 
-        queue.push_back((start, 0));
+        queue.push_back((self.start, 0));
         while !queue.is_empty() {
             let (position, distance) = queue.pop_front().unwrap();
             match distances[position.0][position.1] {
                 Some(v) if v <= distance => {}
                 _ => {
-                    distances[position.0][position.1] = Some(distance);
+                    if self.rows[position.0][position.1] == b'a' {
+                        distances[position.0][position.1] = Some(0)
+                    } else {
+                        distances[position.0][position.1] = Some(distance);
+                    }
+
                     if position.0 > 0
                         && self.rows[position.0 - 1][position.1]
                             <= self.rows[position.0][position.1] + 1
                     {
-                        queue.push_back(((position.0 - 1, position.1), distance + 1));
+                        queue.push_back((
+                            (position.0 - 1, position.1),
+                            distances[position.0][position.1].unwrap() + 1,
+                        ));
                     }
                     if position.0 < self.height - 1
                         && self.rows[position.0 + 1][position.1]
                             <= self.rows[position.0][position.1] + 1
                     {
-                        queue.push_back(((position.0 + 1, position.1), distance + 1));
+                        queue.push_back((
+                            (position.0 + 1, position.1),
+                            distances[position.0][position.1].unwrap() + 1,
+                        ));
                     }
                     if position.1 > 0
                         && self.rows[position.0][position.1 - 1]
                             <= self.rows[position.0][position.1] + 1
                     {
-                        queue.push_back(((position.0, position.1 - 1), distance + 1));
+                        queue.push_back((
+                            (position.0, position.1 - 1),
+                            distances[position.0][position.1].unwrap() + 1,
+                        ));
                     }
                     if position.1 < self.width - 1
                         && self.rows[position.0][position.1 + 1]
                             <= self.rows[position.0][position.1] + 1
                     {
-                        queue.push_back(((position.0, position.1 + 1), distance + 1));
+                        queue.push_back((
+                            (position.0, position.1 + 1),
+                            distances[position.0][position.1].unwrap() + 1,
+                        ));
                     }
                 }
             }
         }
 
-        distances[self.end.0][self.end.1]
+        distances[self.end.0][self.end.1].unwrap()
     }
 }
 
@@ -101,15 +112,7 @@ fn main() {
 
 fn run(input: &str) -> usize {
     let map = Map::new_from_input(input);
-    let mut result = usize::MAX;
-
-    for start in map.find_all_start_positions() {
-        if let Some(v) = map.find_shortest_path(start) {
-            result = usize::min(result, v);
-        }
-    }
-
-    result
+    map.find_shortest_path()
 }
 
 #[cfg(test)]
